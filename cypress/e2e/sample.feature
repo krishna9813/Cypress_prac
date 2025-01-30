91e5389a396dc232a20f9c4d6f2bca64ca8d578d
Abhinav Krishna Balaji <abalaji2@eu.trimblecorp.net>
2025-01-30 11:02:17 +0530

    First release of Hello world

---
+++ cypress/e2e/sample.feature
@@ -0,0 +1,19 @@
+@regression
+Feature: Sample feature
+Background: 
+      Given open sample application
+@smoke @sanity
+Scenario: Home Page
+    When I log in as Following
+      |userName| password |
+      |mercury| mercury |
+    Then click on submit button
+    And Screen should display 'Login Successfully'
+   
+
+Scenario: verifying the title 
+    When I log in as Following
+      |userName| password |
+      |mercury| mercury |
+    Then click on submit button
+    And Screen should display 'Login Successfully'
---
+++ cypress/e2e/sample/sample.ts
@@ -0,0 +1,32 @@
+import { Given, When,Then, Before} from '@badeball/cypress-cucumber-preprocessor';
+Before(()=>{
+
+    cy.wait(1000)
+})
+after(()=>{
+    cy.wait(1000)
+})
+
+Given("open sample application",()=>{
+    cy.visit("https://demo.guru99.com/test/newtours")
+})
+
+//When("provide valid username and password",()=>{
+ //  cy.get("input[name='userName']").type('mercury')
+ //  cy.get("input[name='password']").type('mercury')
+//})
+When("I log in as Following",(datatable:any)=>{
+    datatable.hashes().forEach(element => {
+        cy.get("input[name='userName']").type(element.userName)
+        cy.get("input[name='password']").type(element.password)
+    });
+})
+
+Then("click on submit button",()=>{
+    cy.get("[name=submit]").click()
+   
+})
+Then("Screen should display {string}",(title)=>{
+    cy.get('table>tbody>tr>td>h3').should('have.text',title)
+
+})
